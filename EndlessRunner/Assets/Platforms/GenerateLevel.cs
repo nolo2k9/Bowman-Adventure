72677085757e8fbd006428befed07c0188d40d8a
Keith Nolan <g00351932@gmit.ie>
2020-11-14 11:56:40 +0000

    Got player moving, jumping and attacking. Generating world now

---
+++ EndlessRunner/Assets/Platforms/GenerateLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateLevel : MonoBehaviour
+{
+    public GameObject[] platforms;
+
+    void Start()
+    {
+       
+       Vector3 position = new Vector3(0,0,0);
+       for (int i=0; i < 20; i++)
+       {
+           int platformNumber = Random.Range(0, platforms.Length);
+           Instantiate(platforms[platformNumber],position, Quaternion.identity);
+           position.z +=10;
+       }
+    }
+
+}
---
+++ EndlessRunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    void StopAttacking()
+    {
+        anim.SetBool("isAttacking", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("isJumping", true);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.SetBool("isJumping", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            anim.SetBool("isAttacking", true);
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            anim.SetBool("isAttacking", false);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(Vector3.up * 90);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(Vector3.up * -90);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(-5 * Time.deltaTime, 0, 0); 
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(5 * Time.deltaTime, 0, 0); 
+        }
+    }
+}
