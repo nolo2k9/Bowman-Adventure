9419d96d39632a2c2e6bcf5094d6f33b735375c1
Keith Nolan <g00351932@gmit.ie>
2020-11-15 12:56:10 +0000

    Added code to delete platforms after they have been used by player

---
+++ EndlessRunner/Assets/Deactivate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deactivate : MonoBehaviour
+{
+    void OnCollisionExit(Collision player) {
+        if(player.gameObject.tag =="Player")
+        {
+            Invoke("RemovePlatform", 3.0f);
+        }
+        
+    }
+
+    void RemovePlatform(){
+        this.gameObject.SetActive(false);
+    }
+}
