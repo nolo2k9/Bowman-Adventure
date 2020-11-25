c1d14206a68ec39c3562354c51b9d5a1ec1bd751
Keith Nolan <g00351932@gmit.ie>
2020-11-25 19:33:37 +0000

    Added coins for picking up

---
+++ EndlessRunner/Assets/coin/pickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other) {
+
+        if(other.gameObject.tag =="Player"){
+            Debug.Log("Pick-up");
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
