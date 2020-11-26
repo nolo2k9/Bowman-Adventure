bcfffccd203832da2ff864a64ed16adc58b5e312
Keith Nolan <g00351932@gmit.ie>
2020-11-26 13:18:18 +0000

    Added UI with some functionality

---
+++ EndlessRunner/Assets/Scripts/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void LoadNewScene(){
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+
+    void Update(){
+        if(Input.GetKey("escape")){
+            EndGame();
+        }
+    }
+
+    
+}
